f10e03dcdb2dfa42b82647a9892acbca029627c8
albertgoncalves <goncalves.albert@gmail.com>
2020-11-22 17:41:55 -0500

    adjust colors

--- src/color.h
+++ src/color.h
@@ -15,20 +15,20 @@ typedef union {
 } Pixel;
 
 #define COLOR_PLAYER_RED   220
-#define COLOR_PLAYER_GREEN 60
-#define COLOR_PLAYER_BLUE  80
+#define COLOR_PLAYER_GREEN 30
+#define COLOR_PLAYER_BLUE  15
 
 #define COLOR_VISIBLE_RED   120
 #define COLOR_VISIBLE_GREEN 120
-#define COLOR_VISIBLE_BLUE  130
+#define COLOR_VISIBLE_BLUE  140
 
-#define COLOR_SHADOW_RED   12
-#define COLOR_SHADOW_GREEN 12
-#define COLOR_SHADOW_BLUE  12
+#define COLOR_SHADOW_RED   14
+#define COLOR_SHADOW_GREEN 14
+#define COLOR_SHADOW_BLUE  18
 
-#define COLOR_WALL_RED   32
-#define COLOR_WALL_GREEN 32
-#define COLOR_WALL_BLUE  38
+#define COLOR_WALL_RED   42
+#define COLOR_WALL_GREEN 42
+#define COLOR_WALL_BLUE  48
 
 static Pixel COLOR_PLAYER;
 static Pixel COLOR_VISIBLE;
@@ -36,18 +36,26 @@ static Pixel COLOR_SHADOW;
 static Pixel COLOR_WALL;
 
 static void set_colors(void) {
-    COLOR_PLAYER.rgb.red = COLOR_PLAYER_RED;
-    COLOR_PLAYER.rgb.green = COLOR_PLAYER_GREEN;
-    COLOR_PLAYER.rgb.blue = COLOR_PLAYER_BLUE;
-    COLOR_VISIBLE.rgb.red = COLOR_VISIBLE_RED;
-    COLOR_VISIBLE.rgb.green = COLOR_VISIBLE_GREEN;
-    COLOR_VISIBLE.rgb.blue = COLOR_VISIBLE_BLUE;
-    COLOR_SHADOW.rgb.red = COLOR_SHADOW_RED;
-    COLOR_SHADOW.rgb.green = COLOR_SHADOW_GREEN;
-    COLOR_SHADOW.rgb.blue = COLOR_SHADOW_BLUE;
-    COLOR_WALL.rgb.red = COLOR_WALL_RED;
-    COLOR_WALL.rgb.green = COLOR_WALL_GREEN;
-    COLOR_WALL.rgb.blue = COLOR_WALL_BLUE;
+    {
+        COLOR_PLAYER.rgb.red = COLOR_PLAYER_RED;
+        COLOR_PLAYER.rgb.green = COLOR_PLAYER_GREEN;
+        COLOR_PLAYER.rgb.blue = COLOR_PLAYER_BLUE;
+    }
+    {
+        COLOR_VISIBLE.rgb.red = COLOR_VISIBLE_RED;
+        COLOR_VISIBLE.rgb.green = COLOR_VISIBLE_GREEN;
+        COLOR_VISIBLE.rgb.blue = COLOR_VISIBLE_BLUE;
+    }
+    {
+        COLOR_SHADOW.rgb.red = COLOR_SHADOW_RED;
+        COLOR_SHADOW.rgb.green = COLOR_SHADOW_GREEN;
+        COLOR_SHADOW.rgb.blue = COLOR_SHADOW_BLUE;
+    }
+    {
+        COLOR_WALL.rgb.red = COLOR_WALL_RED;
+        COLOR_WALL.rgb.green = COLOR_WALL_GREEN;
+        COLOR_WALL.rgb.blue = COLOR_WALL_BLUE;
+    }
 }
 
 #endif
