65aa57d05f4c83ae2bb289620df6344e96f72a40
Albert Goncalves <goncalves.albert@gmail.com>
2020-11-22 11:54:48 -0500

    constraint controls to u/d/l/r, remove global state, pixel union

--- src/main.c
+++ src/main.c
@@ -12,12 +12,6 @@
 static const u32 WINDOW_WIDTH = PX_WIDTH * PX_SCALE;
 static const u32 WINDOW_HEIGHT = PX_HEIGHT * PX_SCALE;
 
-static f32 PLAYER_X = PX_WIDTH / 2.0f;
-static f32 PLAYER_Y = PX_HEIGHT / 2.0f;
-
-static f32 NEXT_PLAYER_X = PX_WIDTH / 2.0f;
-static f32 NEXT_PLAYER_Y = PX_HEIGHT / 2.0f;
-
 static const f32 PX_WIDTH_MINUS_1 = PX_WIDTH - 1.0f;
 static const f32 PX_HEIGHT_MINUS_1 = PX_HEIGHT - 1.0f;
 
@@ -30,19 +24,19 @@ static const f32 FRAME_DURATION = (1.0f / 60.0f) * MILLISECONDS;
 static const u32 FRAME_UPDATE_STEP =
     (u32)(FRAME_DURATION / (f32)FRAME_UPDATE_COUNT);
 
-#define KEY_SENSITIVITY 0.0825;
-
-#define COLOR_BACKGROUND_RED   32
-#define COLOR_BACKGROUND_GREEN 32
-#define COLOR_BACKGROUND_BLUE  32
+#define KEY_SENSITIVITY 0.065;
 
 #define COLOR_PLAYER_RED   220
 #define COLOR_PLAYER_GREEN 60
 #define COLOR_PLAYER_BLUE  80
 
-#define COLOR_WALL_RED   72
-#define COLOR_WALL_GREEN 72
-#define COLOR_WALL_BLUE  72
+#define COLOR_EMPTY_RED   58
+#define COLOR_EMPTY_GREEN 58
+#define COLOR_EMPTY_BLUE  62
+
+#define COLOR_WALL_RED   32
+#define COLOR_WALL_GREEN 32
+#define COLOR_WALL_BLUE  38
 
 typedef struct {
     u32 start;
@@ -58,20 +52,40 @@ typedef struct {
     u8 red;
     u8 green;
     u8 blue;
-    u8 _;
+} Rgb;
+
+typedef union {
+    u32 pack;
+    Rgb rgb;
 } Pixel;
 
+static Pixel COLOR_EMPTY;
+static Pixel COLOR_PLAYER;
+static Pixel COLOR_WALL;
+
 typedef enum {
-    INPUT_DEAD = 1 << 0,
-    INPUT_UP = 1 << 1,
-    INPUT_DOWN = 1 << 2,
-    INPUT_LEFT = 1 << 3,
-    INPUT_RIGHT = 1 << 4,
-} Input;
+    STATE_ALIVE = 0,
+    STATE_DEAD = 1 << 0,
+} State;
+
+typedef enum {
+    DIR_UP = 0,
+    DIR_DOWN,
+    DIR_LEFT,
+    DIR_RIGHT,
+    DIR_COUNT,
+    DIR_NONE,
+} Direction;
 
 typedef struct {
-    Pixel buffer[PX_HEIGHT][PX_WIDTH];
-} Memory;
+    f32 x;
+    f32 y;
+    f32 next_x;
+    f32 next_y;
+    u16 control[DIR_COUNT];
+    // NOTE: It would take a lot of effort, but this *can* roll over.
+    u16 control_counter;
+} Player;
 
 typedef struct {
     u8 x0;
@@ -107,77 +121,66 @@ static const u8 HORIZONTAL_LINES_COUNT =
 static const u8 VERTICAL_LINES_COUNT =
     (u8)(sizeof(VERTICAL_LINES) / sizeof(VERTICAL_LINES[0]));
 
-static const Pixel COLOR_BACKGROUND = {
-    .red = COLOR_BACKGROUND_RED,
-    .green = COLOR_BACKGROUND_GREEN,
-    .blue = COLOR_BACKGROUND_BLUE,
-};
-
-static const Pixel COLOR_PLAYER = {
-    .red = COLOR_PLAYER_RED,
-    .green = COLOR_PLAYER_GREEN,
-    .blue = COLOR_PLAYER_BLUE,
-};
-
-static const Pixel COLOR_WALL = {
-    .red = COLOR_WALL_RED,
-    .green = COLOR_WALL_GREEN,
-    .blue = COLOR_WALL_BLUE,
-};
+typedef struct {
+    Pixel buffer[PX_HEIGHT][PX_WIDTH];
+} Memory;
 
 static f32 clamp_f32(f32 x, f32 min, f32 max) {
     return x < min ? min : max < x ? max : x;
 }
 
-static void set_buffer(Pixel buffer[PX_HEIGHT][PX_WIDTH]) {
+static void set_buffer(Player* player, Pixel buffer[PX_HEIGHT][PX_WIDTH]) {
     for (u32 i = 0; i < PX_HEIGHT; ++i) {
         for (u32 j = 0; j < PX_WIDTH; ++j) {
-            buffer[i][j] = COLOR_BACKGROUND;
+            buffer[i][j].pack = COLOR_EMPTY.pack;
         }
     }
     for (u8 i = 0; i < HORIZONTAL_LINES_COUNT; ++i) {
         HorizontalLine line = HORIZONTAL_LINES[i];
         for (u8 x = line.x0; x < line.x1; ++x) {
-            buffer[line.y][x] = COLOR_WALL;
+            buffer[line.y][x].pack = COLOR_WALL.pack;
         }
     }
     for (u8 i = 0; i < VERTICAL_LINES_COUNT; ++i) {
         VerticalLine line = VERTICAL_LINES[i];
         for (u8 y = line.y0; y < line.y1; ++y) {
-            buffer[y][line.x] = COLOR_WALL;
+            buffer[y][line.x].pack = COLOR_WALL.pack;
         }
     }
-    buffer[(u8)PLAYER_Y][(u8)PLAYER_X] = COLOR_PLAYER;
+    buffer[(u8)player->y][(u8)player->x].pack = COLOR_PLAYER.pack;
 }
 
-static void set_input(u8* input) {
+static void set_state(u8* state, Player* player) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
         case SDL_QUIT: {
-            *input |= INPUT_DEAD;
+            *state |= STATE_DEAD;
             return;
         }
         case SDL_KEYDOWN: {
+            if (event.key.repeat) {
+                continue;
+            }
             switch (event.key.keysym.sym) {
             case SDLK_ESCAPE: {
-                *input |= INPUT_DEAD;
+                *state |= STATE_DEAD;
                 return;
             }
             case SDLK_w: {
-                *input |= INPUT_UP;
+                player->control[DIR_UP] = ++player->control_counter;
                 break;
             }
             case SDLK_s: {
-                *input |= INPUT_DOWN;
+                player->control[DIR_DOWN] = ++player->control_counter;
                 break;
             }
             case SDLK_a: {
-                *input |= INPUT_LEFT;
+                player->control[DIR_LEFT] = ++player->control_counter;
                 break;
             }
             case SDLK_d: {
-                *input |= INPUT_RIGHT;
+                player->control[DIR_RIGHT] = ++player->control_counter;
                 break;
             }
             }
@@ -186,103 +189,114 @@ static void set_input(u8* input) {
         case SDL_KEYUP: {
             switch (event.key.keysym.sym) {
             case SDLK_w: {
-                *input &= (u8)(~INPUT_UP);
+                player->control[DIR_UP] = 0;
                 break;
             }
             case SDLK_s: {
-                *input &= (u8)(~INPUT_DOWN);
+                player->control[DIR_DOWN] = 0;
                 break;
             }
             case SDLK_a: {
-                *input &= (u8)(~INPUT_LEFT);
+                player->control[DIR_LEFT] = 0;
                 break;
             }
             case SDLK_d: {
-                *input &= (u8)(~INPUT_RIGHT);
+                player->control[DIR_RIGHT] = 0;
                 break;
             }
             }
             break;
         }
         }
+        if ((player->control[0] + player->control[1] + player->control[2] +
+             player->control[3]) == 0)
+        {
+            player->control_counter = 0;
+        }
     }
 }
 
-static void update_player_position(Pixel buffer[PX_HEIGHT][PX_WIDTH]) {
-    NEXT_PLAYER_X = clamp_f32(NEXT_PLAYER_X, 0.0f, PX_WIDTH_MINUS_1);
-    NEXT_PLAYER_Y = clamp_f32(NEXT_PLAYER_Y, 0.0f, PX_HEIGHT_MINUS_1);
-    {
-        Pixel next_pixel = buffer[(u8)NEXT_PLAYER_Y][(u8)NEXT_PLAYER_X];
-        if ((next_pixel.red != COLOR_WALL.red) ||
-            (next_pixel.green != COLOR_WALL.green) ||
-            (next_pixel.blue != COLOR_WALL.blue))
-        {
-            PLAYER_X = NEXT_PLAYER_X;
-            PLAYER_Y = NEXT_PLAYER_Y;
-            return;
-        }
+static void update_player_position(Player* player,
+                                   Pixel   buffer[PX_HEIGHT][PX_WIDTH]) {
+    player->next_x = clamp_f32(player->next_x, 0.0f, PX_WIDTH_MINUS_1);
+    player->next_y = clamp_f32(player->next_y, 0.0f, PX_HEIGHT_MINUS_1);
+    Pixel next_pixel = buffer[(u8)player->next_y][(u8)player->next_x];
+    if (next_pixel.pack != COLOR_WALL.pack) {
+        player->x = player->next_x;
+        player->y = player->next_y;
+        return;
     }
-    // NOTE: Not the best solution. Would be nicer to simply prevent player
-    // from being able to move diagonally.
-    {
-        Pixel next_pixel = buffer[(u8)PLAYER_Y][(u8)NEXT_PLAYER_X];
-        if ((next_pixel.red != COLOR_WALL.red) ||
-            (next_pixel.green != COLOR_WALL.green) ||
-            (next_pixel.blue != COLOR_WALL.blue))
-        {
-            PLAYER_X = NEXT_PLAYER_X;
-            NEXT_PLAYER_Y = PLAYER_Y;
-            return;
-        }
-    }
-    {
-        Pixel next_pixel = buffer[(u8)NEXT_PLAYER_Y][(u8)PLAYER_X];
-        if ((next_pixel.red != COLOR_WALL.red) ||
-            (next_pixel.green != COLOR_WALL.green) ||
-            (next_pixel.blue != COLOR_WALL.blue))
-        {
-            NEXT_PLAYER_X = PLAYER_X;
-            PLAYER_Y = NEXT_PLAYER_Y;
-            return;
-        }
-    }
-    NEXT_PLAYER_X = PLAYER_X;
-    NEXT_PLAYER_Y = PLAYER_Y;
+    player->next_x = player->x;
+    player->next_y = player->y;
 }
 
-static void update_frame(Frame* frame,
-                         u8     input,
-                         Pixel  buffer[PX_HEIGHT][PX_WIDTH]) {
+static Direction get_direction(Player* player) {
+    u16       max = 0;
+    Direction direction = DIR_NONE;
+    for (u8 i = 0; i < DIR_COUNT; ++i) {
+        if (max < player->control[i]) {
+            max = player->control[i];
+            direction = (Direction)i;
+        }
+    }
+    return direction;
+}
+
+static void update_frame(Frame*  frame,
+                         Player* player,
+                         Pixel   buffer[PX_HEIGHT][PX_WIDTH]) {
     frame->delta += frame->start - frame->prev;
     while (FRAME_UPDATE_STEP < frame->delta) {
-        if (input & INPUT_UP) {
-            NEXT_PLAYER_Y -= KEY_SENSITIVITY;
+        switch (get_direction(player)) {
+        case DIR_UP: {
+            player->next_y -= KEY_SENSITIVITY;
+            break;
         }
-        if (input & INPUT_DOWN) {
-            NEXT_PLAYER_Y += KEY_SENSITIVITY;
+        case DIR_DOWN: {
+            player->next_y += KEY_SENSITIVITY;
+            break;
         }
-        if (input & INPUT_LEFT) {
-            NEXT_PLAYER_X -= KEY_SENSITIVITY;
+        case DIR_LEFT: {
+            player->next_x -= KEY_SENSITIVITY;
+            break;
         }
-        if (input & INPUT_RIGHT) {
-            NEXT_PLAYER_X += KEY_SENSITIVITY;
+        case DIR_RIGHT: {
+            player->next_x += KEY_SENSITIVITY;
+            break;
         }
-        update_player_position(buffer);
+        case DIR_COUNT:
+        case DIR_NONE: {
+            break;
+        }
+        }
+        update_player_position(player, buffer);
         frame->delta -= FRAME_UPDATE_STEP;
         ++frame->update_count;
     }
     frame->prev = frame->start;
 }
 
-static void debug_frame(Frame* frame) {
+static void debug_frame(Frame* frame, Player* player) {
     frame->end = SDL_GetTicks();
     if (FRAME_DEBUG_INTERVAL <= ++frame->fps_count) {
-        printf("\033[2A"
-               "frames  / sec.  :%6.2f\n"
-               "updates / frame :%6.2f\n",
+        printf("\033[8A"
+               "frames  / sec.       :%6.2f\n"
+               "updates / frame      :%6.2f\n"
+               "player.x             :%6.2f\n"
+               "player.y             :%6.2f\n"
+               "player.control.up    :%6hu\n"
+               "player.control.down  :%6hu\n"
+               "player.control.left  :%6hu\n"
+               "player.control.right :%6hu\n",
                (f32)frame->fps_count / (f32)(frame->end - frame->fps_start) *
                    MILLISECONDS,
-               (f32)frame->update_count / (f32)FRAME_DEBUG_INTERVAL);
+               (f32)frame->update_count / (f32)FRAME_DEBUG_INTERVAL,
+               player->x,
+               player->y,
+               player->control[DIR_UP],
+               player->control[DIR_DOWN],
+               player->control[DIR_LEFT],
+               player->control[DIR_RIGHT]);
         frame->fps_start = frame->start;
         frame->fps_count = 0;
         frame->update_count = 0;
@@ -294,17 +308,22 @@ static const u32 TEXTURE_WIDTH = PX_WIDTH * sizeof(Pixel);
 static void loop(SDL_Renderer* renderer,
                  SDL_Texture*  texture,
                  Pixel         buffer[PX_HEIGHT][PX_WIDTH]) {
-    Frame frame = {0};
-    u8    input = 0;
-    printf("\n\n");
+    Frame  frame = {0};
+    u8     state = 0;
+    Player player = {0};
+    player.x = PX_WIDTH / 2.0f;
+    player.y = PX_HEIGHT / 2.0f;
+    player.next_x = player.x;
+    player.next_y = player.y;
+    printf("\n\n\n\n\n\n\n\n");
     for (;;) {
         frame.start = SDL_GetTicks();
-        set_input(&input);
-        if (input & INPUT_DEAD) {
+        set_state(&state, &player);
+        if (state & STATE_DEAD) {
             return;
         }
-        update_frame(&frame, input, buffer);
-        set_buffer(buffer);
+        update_frame(&frame, &player, buffer);
+        set_buffer(&player, buffer);
         {
             if (SDL_RenderClear(renderer) < 0) {
                 ERROR("SDL_RenderClear(...) < 0");
@@ -317,23 +336,38 @@ static void loop(SDL_Renderer* renderer,
             }
             SDL_RenderPresent(renderer);
         }
-        debug_frame(&frame);
+        debug_frame(&frame, &player);
     }
 }
 
 i32 main(void) {
-    printf("sizeof(Frame)  : %zu\n"
-           "sizeof(Pixel)  : %zu\n"
-           "sizeof(Input)  : %zu\n"
-           "sizeof(Memory) : %zu\n\n",
+    printf("sizeof(Frame)     : %zu\n"
+           "sizeof(Rgb)       : %zu\n"
+           "sizeof(Pixel)     : %zu\n"
+           "sizeof(State)     : %zu\n"
+           "sizeof(Direction) : %zu\n"
+           "sizeof(Player)    : %zu\n"
+           "sizeof(Memory)    : %zu\n\n",
            sizeof(Frame),
+           sizeof(Rgb),
            sizeof(Pixel),
-           sizeof(Input),
+           sizeof(State),
+           sizeof(Direction),
+           sizeof(Player),
            sizeof(Memory));
     Memory* memory = calloc(1, sizeof(Memory));
     if (!memory) {
         ERROR("!memory");
     }
+    COLOR_EMPTY.rgb.red = COLOR_EMPTY_RED;
+    COLOR_EMPTY.rgb.green = COLOR_EMPTY_GREEN;
+    COLOR_EMPTY.rgb.blue = COLOR_EMPTY_BLUE;
+    COLOR_PLAYER.rgb.red = COLOR_PLAYER_RED;
+    COLOR_PLAYER.rgb.green = COLOR_PLAYER_GREEN;
+    COLOR_PLAYER.rgb.blue = COLOR_PLAYER_BLUE;
+    COLOR_WALL.rgb.red = COLOR_WALL_RED;
+    COLOR_WALL.rgb.green = COLOR_WALL_GREEN;
+    COLOR_WALL.rgb.blue = COLOR_WALL_BLUE;
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         ERROR("SDL_Init(...) < 0");
     }
