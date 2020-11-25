05c942521a5a72f18ad005f45451a5576fd11057
albertgoncalves <goncalves.albert@gmail.com>
2020-11-24 19:22:41 -0500

    adjust colors

--- src/color.h
+++ src/color.h
@@ -18,16 +18,16 @@ static const Pixel COLOR_PLAYER = {
     .rgb = {.red = 220, .green = 30, .blue = 15},
 };
 static const Pixel COLOR_EMPTY = {
-    .rgb = {.red = 120, .green = 120, .blue = 130},
+    .rgb = {.red = 70, .green = 70, .blue = 105},
 };
 static const Pixel COLOR_EMPTY_SHADOW = {
-    .rgb = {.red = 30, .green = 30, .blue = 40},
+    .rgb = {.red = 15, .green = 15, .blue = 25},
 };
 static const Pixel COLOR_WALL = {
-    .rgb = {.red = 70, .green = 70, .blue = 80},
+    .rgb = {.red = 120, .green = 120, .blue = 155},
 };
 static const Pixel COLOR_WALL_SHADOW = {
-    .rgb = {.red = 15, .green = 15, .blue = 25},
+    .rgb = {.red = 30, .green = 30, .blue = 40},
 };
 
 #endif
